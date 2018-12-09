e4dd498e8fcf4938bb955ece98b862db1cba29f4
Nuno Nogueira <1111785@isep.ipp.pt>
2018-12-09 19:46:16 +0000

    it2 azure

---
+++ models/ItemProduct.js
@@ -0,0 +1,36 @@
+var mongoose = require('mongoose');
+var Schema = mongoose.Schema;
+
+var ItemProductSchema = new Schema({
+    id: Number,
+    productId: { 
+        type: Number, 
+        min: 1,
+        require: true
+    },
+    name: String,
+    price: { 
+        min: 0,
+        require: true,
+        type: Number
+    },
+    childrenProducts: [{type: Schema.Types.ObjectId, ref: 'ItemProduct'}],
+    width: { 
+        type: Number, 
+        min: 0,
+        require: true
+    },
+    height: { 
+        type: Number, 
+        min: 0,
+        require: true
+    },
+    depth: { 
+        type: Number, 
+        min: 0,
+        require: true
+    },
+});
+
+
+module.exports = mongoose.model('ItemProduct', ItemProductSchema);
